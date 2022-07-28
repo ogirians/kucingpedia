9bec033fb21e098208cdcd5110030992bd0061e3
ogirians <ogi.rians3@gmail.com>
2022-07-28 14:06:29 +0700

    welcome page

---
+++ lib/home_page.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+// import 'package:kucingpedia/detail_page.dart';
+final ButtonStyle flatButtonStyle = TextButton.styleFrom(
+  primary: Colors.white,
+  backgroundColor: Colors.lightBlue,
+  minimumSize: Size(88, 36),
+  padding: EdgeInsets.symmetric(horizontal: 16.0),
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+  ),
+);
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required String title, }) : super(key: key);
+  
+
+
+
+  @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+          body: Padding(
+                padding: EdgeInsets.all(10.0),   
+                child: (
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center, //Center Row contents horizontally,
+                    crossAxisAlignment: CrossAxisAlignment.center, //Center Row contents vertically,
+                    children: <Widget>[
+                      Container(
+                        // color: Colors.grey[100],
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center, //Center Column contents vertically,
+                          crossAxisAlignment: CrossAxisAlignment.center, //Center Column contents horizontally,
+                          children: <Widget>[
+                            const Text(
+                              'Gambar Kucing.png',
+                              style : TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                              )
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(  
+                        // color: Colors.grey[100],
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center, //Center Column contents vertically,
+                          crossAxisAlignment: CrossAxisAlignment.center, //Center Column contents horizontally,
+                          children: <Widget>[
+                            const Text(
+                              'Cari tahu tentang Kucing kamu',
+                              textAlign: TextAlign.center,
+                              style : TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                                                                
+                              )
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(  
+                        // color: Colors.grey[100],
+                        padding: EdgeInsets.only(top:15),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center, //Center Column contents vertically,
+                          crossAxisAlignment: CrossAxisAlignment.center, //Center Column contents horizontally,
+                          children: <Widget>[
+                            const Text(
+                              'Ensiklopedia tentang kucing-kucing di dunia , dan media sharing',
+                              textAlign: TextAlign.center,
+                              style : TextStyle(
+                                color: Colors.black45,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                                                
+                              )
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        // color: Colors.grey[100],
+                        padding: EdgeInsets.only(top: 30),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center, //Center Column contents vertically,
+                          crossAxisAlignment: CrossAxisAlignment.center, //Center Column contents horizontally,
+                          children: <Widget>[
+                            TextButton(
+                              style: flatButtonStyle,
+                              onPressed: () { },
+                              child: Text('Masuk'),
+                            )
+
+                          ],
+                        ),
+                      ),
+                      
+                    ],
+                  )
+                  
+                )
+          )
+        );
+      
+  }
+}
+
+
+
